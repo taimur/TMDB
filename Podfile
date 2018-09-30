6b2ba8775ed420cea7cf60ef5964733663a60970
TaimurAjmal <taimur.ajmal@gmail.com>
2018-09-30 17:22:30 +0500

    Pod configured

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TMDB' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TMDB
+   pod 'Alamofire', '~> 4.7'
+
+  target 'TMDBTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TMDBUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ TMDB/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Taimur Ajmal on 9/29/18.
+//  Copyright © 2018 Taimur Ajmal. All rights reserved.
+//
+
+import UIKit
+
+class TMDBMoviesViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
