967cd4d57a837bffb2abc27ca2e7616b02996f33
TaimurAjmal <taimur.ajmal@gmail.com>
2018-10-02 01:02:50 +0500

    Table view to display choices

--- TMDB/Controllers/SuggestedValuesTVC.swift
+++ TMDB/Controllers/SuggestedValuesTVC.swift
@@ -1,95 +1,41 @@
-//
-//  SuggestedValuesTVC.swift
-//  TMDB
-//
-//  Created by Taimur Ajmal on 10/2/18.
-//  Copyright © 2018 Taimur Ajmal. All rights reserved.
-//
+// Copyright 2018, Ralf Ebert
+// License   https://opensource.org/licenses/MIT
+// License   https://creativecommons.org/publicdomain/zero/1.0/
+// Source    https://www.ralfebert.de/ios-examples/uikit/choicepopover/
 
 import UIKit
 
 class SuggestedValuesTVC: UITableViewController {
+  typealias SelectionHandler = (String) -> Void
+  typealias LabelProvider = (String) -> String
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  private let values : [String]
+  private let labels : LabelProvider
+  private let onSelect : SelectionHandler?
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+  init(_ values : [String], labels : @escaping LabelProvider = String.init(describing:), onSelect : SelectionHandler? = nil) {
+    self.values = values
+    self.onSelect = onSelect
+    self.labels = labels
+    super.init(style: .plain)
+  }
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return values.count
+  }
 
-    // MARK: - Table view data source
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+    cell.textLabel?.text = labels(values[indexPath.row])
+    return cell
+  }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    self.dismiss(animated: true)
+    onSelect?(values[indexPath.row])
+  }
 }
