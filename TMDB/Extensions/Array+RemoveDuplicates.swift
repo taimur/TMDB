052b48a77d13402ddb8ac708be161d06df5a73a3
TaimurAjmal <taimur.ajmal@gmail.com>
2018-10-02 03:34:06 +0500

    Remove duplicates

--- TMDB/Extensions/Array+RemoveDuplicates.swift
+++ TMDB/Extensions/Array+RemoveDuplicates.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+extension Array where Element:Equatable {
+  func removeDuplicates() -> [Element] {
+    var result = [Element]()
+
+    for value in self {
+      if result.contains(value) == false {
+        result.append(value)
+      }
+    }
+
+    return result
+  }
+}
