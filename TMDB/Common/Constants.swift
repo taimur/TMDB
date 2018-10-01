0aae63c47a8b763f7208b0938a3139c0796d61ef
TaimurAjmal <taimur.ajmal@gmail.com>
2018-10-02 01:02:13 +0500

    Constants for global values

---
+++ TMDB/Common/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TMDB
+//
+//  Created by Taimur Ajmal on 10/1/18.
+//  Copyright © 2018 Taimur Ajmal. All rights reserved.
+//
+
+import Foundation
+
+let ERROR_TITLE = "Error"
+let GENERAL_ERROR_DESC = "Something went wrong!, Please try agian"
+let NO_MOVIE_FOUND = "No Movies found, Please try with different title"
