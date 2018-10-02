41f0b243f14f026626b7c98ef92595bababfa659
TaimurAjmal <taimur.ajmal@gmail.com>
2018-10-02 14:43:19 +0500

    Remove Duplicates

--- TMDB/Extensions/Array+RemoveDuplicates.swift
+++ TMDB/Extensions/Array+RemoveDuplicates.swift
@@ -8,16 +8,12 @@
 
 import Foundation
 
-extension Array where Element:Equatable {
-  func removeDuplicates() -> [Element] {
-    var result = [Element]()
-
-    for value in self {
-      if result.contains(value) == false {
-        result.append(value)
+extension Array where Element: Equatable {
+  func removeDuplicates() -> Array {
+    return reduce(into: []) { result, element in
+      if !result.contains(element) {
+        result.append(element)
       }
     }
-
-    return result
   }
 }
